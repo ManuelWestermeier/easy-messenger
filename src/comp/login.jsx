4feea49fc3c787496a86fe898c1460785cd7d4a2
ManuelWestermeier <westermeier111@gmail.com>
2025-03-06 14:35:44 +0100

    headline 1 chnage

--- src/comp/login.jsx
+++ src/comp/login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Login({ handleLogin }) {
   return (
     <>
-      <h1>QUATSCHER Messenger</h1>
+      <h1 className="opensource-info">QUATSCHER Messenger</h1>
       <fieldset className="login">
         <legend>
           <h1>Login</h1>
