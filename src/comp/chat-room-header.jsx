eaf8fb6dd9fa4c4e73281da4c61fe883848cafb5
ManuelWestermeier <westermeier111@gmail.com>
2025-03-08 11:55:14 +0100

    icons update

--- src/comp/chat-room-header.jsx
+++ src/comp/chat-room-header.jsx
@@ -4,13 +4,15 @@ export function ChatRoomHeader({ chatId, chatData, setData, client }) {
   return (
     <header key={chatId}>
       <h3>{chatData?.chatName}</h3>
+
       <button
         type="button"
+        title="Chat Settings"
         onClick={(_) =>
           document.getElementById("chat-data-form").classList.toggle("none")
         }
       >
-        Edit
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z" /></svg>
       </button>
       <form
         className="none"
@@ -37,16 +39,19 @@ export function ChatRoomHeader({ chatId, chatData, setData, client }) {
         }}
       >
         <button
+          className="danger"
           type="reset"
           onClick={(e) => {
             e.preventDefault();
-            e.target.parentElement.classList.add("none");
+            document.getElementById("chat-data-form").classList.add("none");
           }}
+          title="Close"
         >
-          Close
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
         </button>
         <hr />
         <button
+          className="danger"
           type="button"
           onClick={async (e) => {
             e.preventDefault();
@@ -65,7 +70,7 @@ export function ChatRoomHeader({ chatId, chatData, setData, client }) {
             e.target.parentElement.classList.add("none");
           }}
         >
-          Delete All Messages
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg> Delete All Messages
         </button>
         <hr />
         <input
@@ -74,7 +79,7 @@ export function ChatRoomHeader({ chatId, chatData, setData, client }) {
           placeholder="name..."
           defaultValue={chatData.author}
         />
-        <button type="submit">change data</button>
+        <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-160v-80h400v80H280Zm160-160v-327L336-544l-56-56 200-200 200 200-56 56-104-103v327h-80Z" /></svg>Update Data</button>
         <hr />
         <input
           name="password"
@@ -82,13 +87,14 @@ export function ChatRoomHeader({ chatId, chatData, setData, client }) {
           readOnly
           value={chatData.rawPassword}
         />
+        <hr />
+        <QRCode
+          className="qr-code"
+          value={`${chatData.chatName}\n${chatData.rawPassword}`}
+          size={60}
+          tabIndex={-1}
+        />
       </form>
-      <QRCode
-        className="qr-code"
-        value={`${chatData.chatName}\n${chatData.rawPassword}`}
-        size={60}
-        tabIndex={-1}
-      />
     </header>
   );
 }
--- src/comp/nabigation-bar.jsx
+++ src/comp/nabigation-bar.jsx
@@ -7,6 +7,22 @@ export function NavigationBar({
   client,
   setPage,
 }) {
+  const deleteChat = (chatId) => async (e) => {
+    e.preventDefault();
+    if (
+      !confirm(
+        `Are you sure you want to delte the chat "${chats[chatId]?.chatName}" for all users in the chat with all messages?`
+      )
+    )
+      return;
+    if (!(await client.get("delete-chat", chatId)))
+      return alert("chat cant be deleted");
+    setChats((old) => {
+      const newChats = { ...old };
+      delete newChats[chatId];
+      return newChats;
+    });
+  }
   return (
     <nav className="nav-bar">
       <ul>
@@ -14,24 +30,10 @@ export function NavigationBar({
           <li
             key={chatId}
             className={chatId === currentChat ? "active" : ""}
-            onContextMenu={async (e) => {
-              e.preventDefault();
-              if (
-                !confirm(
-                  `Are you sure you want to delte the chat "${chats[chatId]?.chatName}" for all users in the chat with all messages?`
-                )
-              )
-                return;
-              if (!(await client.get("delete-chat", chatId)))
-                return alert("chat cant be deleted");
-              setChats((old) => {
-                const newChats = { ...old };
-                delete newChats[chatId];
-                return newChats;
-              });
-            }}
+            onContextMenu={deleteChat(chatId)}
           >
             <button
+              className="chat-select-button"
               onClick={() => {
                 setCurrentChat(chatId);
                 if (setPage) setPage(false);
@@ -64,6 +66,9 @@ export function NavigationBar({
               >
                 {chats[chatId].unread}
               </span>
+              <button title="delete chat" className="danger" onClick={deleteChat(chatId)}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" /></svg>
+              </button>
             </button>
           </li>
         ))}
