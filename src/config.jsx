58dda7e322bc8f6bd1a1cd6372bcea99596bb5f7
ManuelWestermeier <westermeier111@gmail.com>
2025-03-10 14:21:19 +0100

    to start the server on app open

--- src/config.jsx
+++ src/config.jsx
@@ -5,3 +5,6 @@ const localHostURL = "ws://localhost:8080";
 
 export const serverURL =
   document.location.protocol == "http:" ? localHostURL : remoteServerURL;
+
+const ws = new WebSocket(remoteServerURL);
+ws.onopen = () => ws.close(23);
