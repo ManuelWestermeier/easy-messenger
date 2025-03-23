30eb02614379c088a4278ba47e941403e8833c62
ManuelWestermeier <westermeier111@gmail.com>
2025-03-23 18:52:09 +0100

    fake ad banner

--- src/comp/loading-state.jsx
+++ src/comp/loading-state.jsx
@@ -18,6 +18,15 @@ export default function LoadingState({ state }) {
       <button onClick={() => setSpeed((prev) => prev / 2)}>
         Click This Button To Make It Faster
       </button>
+
+      {/* Fake Advertising Section */}
+      <div style={{ marginTop: "20px", padding: "10px", border: "1px solid #ccc", backgroundColor: "#f9f9f9" }}>
+        <h4>🚀 Boost Your Speed Instantly! 🚀</h4>
+        <p>Upgrade to <b>LoadingPro+</b> and experience 10x faster loading times.</p>
+        <button style={{ backgroundColor: "gold", padding: "5px 10px", border: "none", cursor: "pointer" }}>
+          Upgrade Now!
+        </button>
+      </div>
     </div>
   );
 }
