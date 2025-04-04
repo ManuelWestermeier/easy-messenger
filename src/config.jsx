7bf17c7a99d08bb09390b52bcb7cef097b77c6fd
ManuelWestermeier <westermeier111@gmail.com>
2025-04-04 13:32:31 +0200

    add https://lockchat-github-io.onrender.com

--- src/config.jsx
+++ src/config.jsx
@@ -3,8 +3,11 @@ const remoteServerURL = "wss://easy-messenger.onrender.com";
 // in local debugging or on http
 const localHostURL = "ws://localhost:8080";
 
-export const serverURL =
-  document.location.protocol == "http:" ? localHostURL : remoteServerURL;
+export const serverURL = document.location.origin.includes("lockchat.github.io")
+  ? "https://lockchat-github-io.onrender.com"
+  : document.location.protocol == "http:"
+  ? localHostURL
+  : remoteServerURL;
 
 const ws = new WebSocket(remoteServerURL);
 ws.onopen = () => ws.close(1000);
