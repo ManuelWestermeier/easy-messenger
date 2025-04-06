f18aa3e0f61b25ad9bee285c87afbc92b85e35f9
ManuelWestermeier <westermeier111@gmail.com>
2025-04-06 14:17:08 +0200

    chage to privuschat.github.io

--- src/config.jsx
+++ src/config.jsx
@@ -3,11 +3,11 @@ const remoteServerURL = "wss://easy-messenger.onrender.com";
 // in local debugging or on http
 const localHostURL = "ws://localhost:8080";
 
-export const serverURL = document.location.origin.includes("lockchat.github.io")
-  ? "wss://lockchat-github-io.onrender.com"
+export const serverURL = document.location.origin.includes("privuschat.github.io")
+  ? "wss://easy-messenger-l8ka.onrender.com"
   : document.location.protocol == "http:"
-  ? localHostURL
-  : remoteServerURL;
+    ? localHostURL
+    : remoteServerURL;
 
 const ws = new WebSocket(remoteServerURL);
 ws.onopen = () => ws.close(1000);
